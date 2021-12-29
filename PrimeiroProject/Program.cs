b5181550e9de503d242d2b4754edda2068a85f9d
rolim <rolim_sk8@hotmail.com>
2021-12-29 14:08:38 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args){
+            var a = "Hello ";
+            var b = "Wold!";
+                Console.WriteLine(a + b);
+            }
+}
+}
